0225910d13729d836f6aa6937cb920b963ca892c
Anhdophin <anhdophin@gmail.com>
2026-08-21 07:45:21 +0700

    Update website

--- la-ca-chon-la/app-files/data.js
+++ la-ca-chon-la/app-files/data.js
@@ -1,39 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-img_20260724_183945_00_049",
-      "name": "IMG_20260724_183945_00_049",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1488,
-      "initialViewParameters": {
-        "yaw": 0.11514725843545293,
-        "pitch": 0.07952219222104162,
-        "fov": 1.5828202300267633
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-img_20260724_181038_00_038",
-      "name": "IMG_20260724_181038_00_038",
+      "id": "0-scene-1",
+      "name": "Scene 1",
       "levels": [
         {
           "tileSize": 256,
@@ -59,15 +28,168 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.43766227207487063,
+          "pitch": 0.38412604923891003,
+          "rotation": 7.853981633974483,
+          "target": "1-scene-2"
+        },
+        {
+          "yaw": -0.6254950598959894,
+          "pitch": 0.3752567442503878,
+          "rotation": 4.71238898038469,
+          "target": "2-scene-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-scene-2",
+      "name": "Scene 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3071172685363841,
+          "pitch": 0.18765945972259424,
+          "rotation": 1.5707963267948966,
+          "target": "0-scene-1"
+        },
+        {
+          "yaw": -0.1295098611883052,
+          "pitch": 0.20372798250326518,
+          "rotation": 4.71238898038469,
+          "target": "2-scene-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.05863749800493068,
+          "pitch": -0.19112818887784044,
+          "title": "Đền Vua Hùng",
+          "text": "Công viên Tao Đàn"
+        }
+      ]
+    },
+    {
+      "id": "2-scene-3",
+      "name": "Scene 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.4978104593690489,
+        "pitch": 1.0720296378667538,
+        "fov": 1.5828202300267633
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4741839558057812,
+          "pitch": 1.0122287645932744,
+          "rotation": 6.283185307179586,
+          "target": "0-scene-1"
+        },
+        {
+          "yaw": -1.3898622757656707,
+          "pitch": 1.2249455069579671,
+          "rotation": 4.71238898038469,
+          "target": "1-scene-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-scene-4",
+      "name": "Scene 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.4978104593690489,
+        "pitch": 1.0720296378667538,
+        "fov": 1.5828202300267633
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4741839558057812,
+          "pitch": 1.0122287645932744,
+          "rotation": 6.283185307179586,
+          "target": "0-scene-1"
+        },
+        {
+          "yaw": -1.3898622757656707,
+          "pitch": 1.2249455069579671,
+          "rotation": 4.71238898038469,
+          "target": "1-scene-2"
+        }
+      ],
       "infoHotspots": []
     }
+
   ],
-  "name": "La Ca Chon La",
+  "name": "La Cà Tao Đàn",
   "settings": {
-    "mouseViewMode": "qtvr",
+    "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
